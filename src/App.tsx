499e0394817905ed0df5b9ed37be871bcaeb15e0
MaxiPrantera <maximiliano.prantera@gmail.com>
2023-10-06 21:06:28 -0300

    actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
 import { useEffect, useState } from "react";
-import api, { Weather } from "./api";
+import api, {type Weather } from "./api";
+import type {City} from './types'
+
+
+
+const CITIES: Record<string, City> = {
+  quilmes:{
+  id: 'quilmes',
+  name: 'Quilmes',
+  lat: -34.7539386,
+  lon: -58.3864614,
+}, palermo:{
+  id: 'palermo',
+  name: 'Palermo',
+  lat: 0,
+  lon: 0
+}, colon:{
+  id: 'colon',
+  name: 'Colon',
+  lat: 0,
+  lon: 0
+},bariloche:{
+  id: 'bariloche',
+  name: 'Bariloche',
+  lat: 0,
+  lon: 0
+},varela:{
+  id: 'varela',
+  name: 'Varela',
+  lat: 0,
+  lon: 0
+},
+};
 
 function App() {
   const [status, setStatus] = useState<'pending' | 'resolved'>('pending')
   const [weather, setWeather] = useState<Weather | null>(null)
+  const [city, setCity] = useState<City>(CITIES['quilmes'])
+
+  function handleChangeCity(event: React.ChangeEvent<HTMLSelectElement>) {
+    setCity(CITIES[event.target.value as keyof typeof CITIES])
+  }
 
   useEffect(() => {
-    api.weather.fetch(0, 0).then((weather) => {
+    api.weather.fetch(city).then((weather) => {
       setWeather(weather)
       setStatus('resolved')
     });
-  },[])
+  },[city])
 
   if (status === 'pending') {
     return <div>Loading...</div>
@@ -21,6 +58,13 @@ function App() {
 
   return(
     <main>
+      <select onChange={handleChangeCity} value={city.id}>
+        {Object.values(CITIES).map((city) => (
+          <option key={city.id} value = {city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
       <h1>{weather.city.name}</h1>
       <ul>
         {weather.forecast.map((forecast, index) => (
